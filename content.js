929b29458bf488822428eff633440cc223823683
Thisura Seniya Rathnayake <seniyaratnayake@gmail.com>
2019-09-23 11:56:54 +0530

    Added Youtube URL Detection in iFrames

--- content.js
+++ content.js
@@ -1,19 +1,34 @@
 // /* data is sent to the local server (127.0.0.1:5244) where Activity Monitor (parent app) will be listening*/
 
 function getURL() {
-  var url = window.location.origin;
-  console.log(url);
+  var url = window.location.origin;    
+  
   if (url == "https://www.youtube.com") {
-    var paused = document.getElementsByTagName('video')[0].paused;    
+    var paused = document.getElementsByTagName('video')[0].paused;   
+    var video = null;  
   }
   else {
     var paused = null;    
+    var iframes = document.getElementsByTagName('iframe');
+    var arrayLength = iframes.length;
+    var video = null;
+
+    if (arrayLength > 0) {
+      for (var i = 0; i < arrayLength; i++) {
+        var u = iframes[i].src;      
+        var hasYouTube = (/^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?/.test(u));    
+        if (hasYouTube === true) { 
+          video = true;
+          break; 
+        }        
+      }    
+    }    
   }
-  
+  console.log(url, paused, video);
   $.ajax({
     url:"http://127.0.0.1:5244/tracker/url",
     method:"POST",
-    data:{url: url, browser: "Browser", paused: paused},            
+    data:{url: url, browser: "Browser", paused: paused, video: video},            
   });    
 }
 
