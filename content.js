03649c8555d7d18d6ee1557defcec4f675c7ff28
Thisura Seniya Rathnayake <seniyaratnayake@gmail.com>
2019-11-06 19:48:52 +0530

    Refactor code

--- content.js
+++ content.js
@@ -1,51 +1,51 @@
 // /* data is sent to the local server (127.0.0.1:5244) where Activity Monitor (parent app) will be listening*/
 
 function getURL() {
-  var url = window.location.origin;    
-  
+  var url = window.location.origin;
+
   if (url == "https://www.youtube.com") {
-    var paused = document.getElementsByTagName('video')[0].paused;   
-    var video = null;  
-  }
-  else {
-    var paused = null;    
-    var iframes = document.getElementsByTagName('iframe');
+    var paused = document.getElementsByTagName("video")[0].paused;
+    var video = null;
+  } else {
+    var paused = null;
+    var iframes = document.getElementsByTagName("iframe");
     var arrayLength = iframes.length;
     var video = null;
 
     if (arrayLength > 0) {
       for (var i = 0; i < arrayLength; i++) {
-        var u = iframes[i].src;      
-        var hasYouTube = (/^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?/.test(u));    
-        if (hasYouTube === true) { 
+        var u = iframes[i].src;
+        var hasYouTube = /^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?/.test(
+          u
+        );
+        if (hasYouTube === true) {
           video = true;
-          break; 
-        }        
-      }    
-    }    
+          break;
+        }
+      }
+    }
   }
-  $.ajax({url: "http://127.0.0.1:5244/tracker/isRunning",
+  $.ajax({
+    url: "http://127.0.0.1:5244/tracker/isRunning",
     type: "GET",
-    timeout:500,
+    timeout: 500,
     statusCode: {
-      200: function (running) {
-        if (running === "1" ) {
+      200: function(running) {
+        if (running === "1") {
           console.log("Sending");
           $.ajax({
-            url:"http://127.0.0.1:5244/tracker/url",
-            method:"POST",
-            data:{url: url, browser: "Browser", paused: paused, video: video},            
-          });  
-        }         
-      }                
+            url: "http://127.0.0.1:5244/tracker/url",
+            method: "POST",
+            data: { url: url, browser: "Browser", paused: paused, video: video }
+          });
+        }
+      }
     }
-  }); 
+  });
 }
 
-
-setInterval(function () {
-  if(document.hasFocus()){
+setInterval(function() {
+  if (document.hasFocus()) {
     getURL();
-  }  
+  }
 }, 1000);
-
